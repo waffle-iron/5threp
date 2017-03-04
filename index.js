ea7fd78cb1efd787317ddb7cd72ed2509745c41f
roei zucker <roei zucker>
2017-03-04 23:50:16 +0200

    for heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect("mongodb://localhost:27017/mongodb", function(err, db) {
+MongoClient.connect("mongodb://ds119020.mlab.com:19020/heroku_wkltq4q5", function(err, db) {
   if(!err) {
     console.log("We are connected");
     db.createCollection("test",function(err,collection){console.log(collection)})
@@ -32,7 +32,7 @@ app.get('/do', function(req,res,next){
 })
 
 app.get('/try', function(req,res,next){
-  MongoClient.connect("mongodb://localhost:27017/mongodb", function(err, db) {
+  MongoClient.connect("mongodb://ds119020.mlab.com:19020/heroku_wkltq4q5", function(err, db) {
   if(!err) {
     console.log("We are connected");
       var collection = db.collection('test');
