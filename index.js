4b4228acd8764fd6f1ecc317b5faa464cf0cf4b3
roei zucker <roei zucker>
2017-03-04 21:55:13 +0200

    changed port

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
 });
 
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 8080));
 
 app.use(express.static(__dirname + '/public'));
 
