8b573ec17d496cc3974ac5c1615c26fab054a3bc
roei zucker <roei zucker>
2017-03-04 21:22:52 +0200

    with mongo stuff

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 var express = require('express');
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
+  if(!err) {
+    console.log("We are connected");
+    db.createCollection("test",function(err,collection){console.log(collection)})
+      var collection = db.collection('test');
+      collection.insert({"sucess":true})
+  }
+  else{
+    console.log(err);
+  }
+});
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -16,8 +30,21 @@ app.get('/', function(request, response) {
 app.get('/do', function(req,res,next){
     res.send("dodo")
 })
+
+app.get('/try', function(req,res,next){
+  MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
+  if(!err) {
+    console.log("We are connected");
+      var collection = db.collection('test');
+      collection.findOne({},function(err,item){res.send(item)});
+
+  }
+  else{
+    console.log(err);
+  }
+});
+})
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
