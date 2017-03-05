b1ca5d43ff8525c197b6e0dbf3e6f83a38ed6891
roei zucker <roei zucker>
2017-03-05 15:04:16 +0200

    with distributed angular

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
+    path = require('path'),
+    fs = require('fs');
 var MongoClient = require('mongodb').MongoClient;
+var staticRoot = __dirname + '/';
 
-MongoClient.connect("mongodb://ds119020.mlab.com:19020/heroku_wkltq4q5", function(err, db) {
+// MongoClient.connect("mongodb://ds119020.mlab.com:19020/heroku_wkltq4q5", function(err, db) {
+MongoClient.connect("mongodb://localhost:27017/main", function(err, db) {
+  
   if(!err) {
     console.log("We are connected");
     db.createCollection("test",function(err,collection){console.log(collection)})
@@ -16,16 +21,41 @@ MongoClient.connect("mongodb://ds119020.mlab.com:19020/heroku_wkltq4q5", functio
 
 
 app.set('port', (process.env.PORT || 8080));
+app.use(express.static(staticRoot));
 
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
+
+/// added from angular 2
+app.use(function(req, res, next){
+    console.log('a')
+    // if the request is not html then move along
+    var accept = req.accepts('html', 'json', 'xml');
+    if(accept !== 'html'){
+        console.log('b')
+        return next();
+    }
+    console.log('c')
+    // if the request has a '.' assume that it's for a file, move along
+    var ext = path.extname(req.path);
+    if (ext !== ''){
+        console.log('d')
+        return next();
+    }
+
+    // fs.createReadStream(staticRoot + 'index.html').pipe(res);
+    fs.createReadStream('./index.html').pipe(res);
+
+});
+///
+
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+// app.get('/', function(request, response) {
+//   response.render('pages/index');
+// });
 
 app.get('/do', function(req,res,next){
     res.send("dodo")
